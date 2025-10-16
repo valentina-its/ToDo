84e190168d92527342a77eecceb867ce73914ca2
quattromoriValentina-its <valentina.slaviero@edu-its.it>
2025-10-16 16:48:30 +0200

    aaaa

--- src/route/route.js
+++ src/route/route.js
@@ -2,4 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const loginController = require('../controller/login');
+const registerController = require('../controller/register');
+
 
