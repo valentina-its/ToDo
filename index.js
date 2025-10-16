243c5ff7d123c85e997fccfbd9091ae2546102dc
quattromoriValentina-its <valentina.slaviero@edu-its.it>
2025-10-16 16:52:40 +0200

    messo apposto le importazioni con il db nel file .env

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // main 
+// Carica le variabili d'ambiente
+require('dotenv').config();
+
 // import express
 const express = require('express');
 const app = express();
 const routes = require('./src/route/route');
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// Importa la connessione al database
+const sequelize = require('./src/connection/db');
 
 app.use(express.json()); // Per parsare il body delle richieste JSON
 app.use('/api', routes);
 
+// Avvia il server
 app.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);
 });
